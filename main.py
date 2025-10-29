78aac9e68f28ccfb30810afac6519d348dfa6053
aishu-glitch <aishwarya29115@gmail.com>
2025-10-29 18:48:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+
+st.title("Jarvis")
+
+input_txt=st.text_input("Please enter your queries here...")
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","you are a helpful AI assistant and your name is Jarvis"),
+        ("user","user query:{query}")
+    ]
+)
+
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_txt:
+    st.write(chain.invoke({"query":input_txt}))
